3d93eec728a3c779c8ad7269a9cae318ba4d340c
shakeeb <shakee.zats@gmail.com>
2024-07-03 12:36:19 +0530

    first commit

---
+++ test-deploy.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: test wordpress deploy
+Plugin URI:  https://rapidload.io/
+Description: Makes your site even faster and lighter by automatically removing Unused CSS from your website.
+Version:     1.0.0
+Author:      RapidLoad
+Author URI:  https://rapidload.io/
+*/
+
