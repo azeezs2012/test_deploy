3b6bef4f7b831b0e663a3e6551d78b4beb8709d5
shakeeb <shakee.zats@gmail.com>
2024-07-22 11:43:05 +0530

    add entrypoint sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 # Define variables
-NGINX_CONF_DIR="/opt/bitnami/nginx/conf/vhosts"
-NGINX_CONF="$NGINX_CONF_DIR/pr-1-redirect.conf"
+APACHE_CONF_DIR="/opt/bitnami/apache2/conf/vhosts"
+APACHE_CONF="$APACHE_CONF_DIR/pr-1-redirect.conf"
 
-# Create the Nginx configuration directory if it doesn't exist
-sudo mkdir -p $NGINX_CONF_DIR
+# Create the Apache configuration directory if it doesn't exist
+sudo mkdir -p $APACHE_CONF_DIR
 
-# Create the Nginx configuration file
-cat <<EOL | sudo tee $NGINX_CONF
-server {
-    listen 8081;
-    server_name localhost;
+# Create the Apache configuration file
+cat <<EOL | sudo tee $APACHE_CONF
+<VirtualHost *:8081>
+    ServerName localhost
 
-    location / {
-        return 301 http://pr-1.rapidload.live\$request_uri;
-    }
-}
+    <Location />
+        Redirect permanent / http://pr-1.rapidload.live
+    </Location>
+</VirtualHost>
 EOL
 
-# Test the Nginx configuration
-sudo /opt/bitnami/nginx/sbin/nginx -t
+# Test the Apache configuration
+sudo /opt/bitnami/apache2/bin/apachectl -t
 
-# Reload Nginx to apply the changes
-sudo /opt/bitnami/ctlscript.sh restart nginx
+# Reload Apache to apply the changes
+sudo /opt/bitnami/ctlscript.sh restart apache
